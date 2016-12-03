0b4c40a010fd5815153b303a9fb326a93723496b
ryanburgess <ryanb@netflix.com>
2016-12-03 15:12:14 -0800

    update link script

--- link.js
+++ link.js
@@ -1,13 +1,48 @@
+'use strict';
 const fs = require('fs');
 const prompt = require('prompt');
 const list = fs.readFileSync('./list.json');
 const fullList = JSON.parse(list);
+const categories = [];
+
+// get list of categories
+for (const cats of fullList) {
+  const catName = cats.cat;
+
+  if (categories.indexOf(catName) === -1) {
+    categories.push(catName);
+  }
+}
+
+// prompt schema
+const schema = {
+  properties: {
+    title: {
+      description: 'Link title',
+      pattern: /^[a-zA-Z\s\-]+$/,
+      message: 'Name must be only letters, spaces, or dashes',
+      required: true
+    },
+    url: {
+      description: 'Link URL',
+      pattern: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/,
+      message: 'Must be a valid URL',
+      required: true
+    },
+    category: {
+      description: `Category (${categories})`,
+      pattern: /^(book|video|podcast|article)/,
+      message: 'Must be of the valid categories',
+      required: true
+    }
+  }
+};
 
 // start prompt
 prompt.start();
 
 // prompt questions
-prompt.get(['title', 'url', 'category'], function (err, result) {
+prompt.get(schema, function (err, result) {
    const obj = {'title': result.title.trim(), 'url': result.url.trim(), 'cat': result.category.trim()};
    fullList.push(obj);
    fs.writeFileSync('./list.json', JSON.stringify(fullList, null, 4));
